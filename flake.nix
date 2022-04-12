d5947091fa937d5b31f2b13e2b40a760e776a8a3
Kevin Mullins <kevin@pnotequalnp.com>
2022-04-12 00:22:06 -0700

    Update flake, Cabal file, and CI

--- flake.nix
+++ flake.nix
@@ -18,35 +18,37 @@
           };
         };
       };
-    in
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          };
-          inherit (pkgs.lib) composeExtensions;
-          lib = pkgs.haskell.lib;
-          hs = pkgs.haskell.packages.ghc921.override {
-            overrides = composeExtensions pkgs.haskell.packageOverrides (final: prev: {
-              llvm-hs-pretty = lib.dontCheck (final.callCabal2nix "llvm-hs-pretty" llvm-hs-pretty { });
-              llvm-hs-pure = final.callCabal2nix "llvm-hs-pure" "${llvm-hs}/llvm-hs-pure" { };
+    in flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+        inherit (pkgs.lib) composeExtensions;
+        lib = pkgs.haskell.lib;
+        hs = pkgs.haskell.packages.ghc921.override {
+          overrides = composeExtensions pkgs.haskell.packageOverrides
+            (final: prev: {
+              llvm-hs-pretty = lib.dontCheck
+                (final.callCabal2nix "llvm-hs-pretty" llvm-hs-pretty { });
+              llvm-hs-pure =
+                final.callCabal2nix "llvm-hs-pure" "${llvm-hs}/llvm-hs-pure"
+                { };
             });
-          };
-        in
-        rec {
-          packages = rec {
-            brainfuck = hs.brainfuck;
-            default = brainfuck;
-          };
+        };
+      in rec {
+        packages = rec {
+          brainfuck = hs.brainfuck;
+          default = brainfuck;
+        };
 
-          apps = rec {
-            brainfuck = flake-utils.lib.mkApp { drv = packages.brainfuck; };
-            default = brainfuck;
-          };
+        apps = rec {
+          brainfuck = flake-utils.lib.mkApp { drv = packages.brainfuck; };
+          default = brainfuck;
+        };
 
-          devShells.default = hs.shellFor {
+        devShells = {
+          default = hs.shellFor {
             packages = hsPkgs: with hsPkgs; [ brainfuck ];
             nativeBuildInputs = with hs; [
               cabal-install
@@ -57,7 +59,12 @@
               pkgs.llvmPackages_12.llvm
             ];
           };
-        }) // {
-      overlays.default = overlay;
-    };
+          ci = hs.shellFor {
+            packages = hsPkgs: with hsPkgs; [ brainfuck ];
+            nativeBuildInputs = with hs; [ cabal-install fourmolu pkgs.nixfmt ];
+          };
+        };
+      }) // {
+        overlays.default = overlay;
+      };
 }
